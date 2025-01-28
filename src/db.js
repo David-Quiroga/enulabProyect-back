4d0d05015ef1946cde975f46033024de699c9bdc
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2025-01-28 18:04:02 -0500

    Base conectada

--- src/db.js
+++ src/db.js
@@ -1,1 +1,9 @@
+import pg  from 'pg'
 
+export const pool  = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    password: "Campos0430",
+    database: "enulab",
+    port: "5433"
+})
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,29 @@
+//! son de prueba
+import { Router } from 'express'
+import { pool } from '../db.js'
+
+const router = Router()
+
+router.get('/user', async (req, res) => {
+
+    const {rows} = await pool.query('SELECT * FROM users')
+    res.json(rows);    
+})
+
+router.get('/user/:id', (req, res) => {
+    const { id } = req.params
+    res.send('Holap', + id)
+})
+
+router.post('/user', (req, res) => {
+    res.send('Crea un usuario')
+})
+
+router.delete('/user/:id', (req, res) => {
+    res.send('Holap')
+})
+
+router.put('/user/:id', (req, res) => {
+    res.send('Holap')
+})
+export default router;
