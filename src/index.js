6e3aebd7113c6887fc5a5f5e3b85cb82bbb928fd
Bsteven593 <bsm.latacumba@yavirac.edu.ec>
2025-02-04 15:23:11 -0500

    cors para unir el front

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import express from 'express';
+import cors from 'cors';  // Importa el paquete cors
 import { PORT } from './config.js';
 import menuRoutes from './routes/menuRoutes.js';
 import dishRoutes from './routes/dishesRoutes.js';
 import dessertRoutes from './routes/dessertRoutes.js';
 import drinkRoutes from './routes/drinkRoutes.js';
-import restaurantRoutes from './routes/restaurantRoutes.js'
-import employeeRoutes from './routes/employeeRoutes.js'
-import inventoryRoutes from './routes/inventoryRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import supplierRoutes from './routes/supplierRoutes.js'
-import food_typeRoutes from './routes/food_typeRoutes.js'
+import restaurantRoutes from './routes/restaurantRoutes.js';
+import employeeRoutes from './routes/employeeRoutes.js';
+import inventoryRoutes from './routes/inventoryRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import supplierRoutes from './routes/supplierRoutes.js';
+import food_typeRoutes from './routes/food_typeRoutes.js';
 import listEndpoints from 'express-list-endpoints'; //!Se usa para ver los endpoints
 import morgan from 'morgan';
 
-
 const app = express();
+
+// Configura CORS para permitir solicitudes desde tu frontend
+app.use(cors({
+    origin: 'http://localhost:3000',  // Cambia la URL de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir encabezados personalizados
+}));
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -25,11 +32,11 @@ app.use('/api', dishRoutes);
 app.use('/api', dessertRoutes);
 app.use('/api', drinkRoutes);
 app.use('/api', restaurantRoutes);
-app.use('/api', employeeRoutes)
-app.use('/api', inventoryRoutes)
-app.use('/api', userRoutes)
-app.use('/api', supplierRoutes)
-app.use('/api', food_typeRoutes)
+app.use('/api', employeeRoutes);
+app.use('/api', inventoryRoutes);
+app.use('/api', userRoutes);
+app.use('/api', supplierRoutes);
+app.use('/api', food_typeRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
