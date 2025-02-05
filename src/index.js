5466e280a6a323b0b709798f902dbd7406646af2
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2025-02-04 20:43:31 -0500

    Se guarda la imagen

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 import express from 'express';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { PORT } from './config.js';
 import menuRoutes from './routes/menuRoutes.js';
 import dishRoutes from './routes/dishesRoutes.js';
 import dessertRoutes from './routes/dessertRoutes.js';
 import drinkRoutes from './routes/drinkRoutes.js';
-import restaurantRoutes from './routes/restaurantRoutes.js'
-import employeeRoutes from './routes/employeeRoutes.js'
-import inventoryRoutes from './routes/inventoryRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import supplierRoutes from './routes/supplierRoutes.js'
-import food_typeRoutes from './routes/food_typeRoutes.js'
-import reserveRoutes from './routes/reserveRouter.js'
-import commentRoutes from './routes/commentRoutes.js'
-import listEndpoints from 'express-list-endpoints'; //!Se usa para ver los endpoints
+import restaurantRoutes from './routes/restaurantRoutes.js';
+import employeeRoutes from './routes/employeeRoutes.js';
+import inventoryRoutes from './routes/inventoryRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import supplierRoutes from './routes/supplierRoutes.js';
+import food_typeRoutes from './routes/food_typeRoutes.js';
+import reserveRoutes from './routes/reserveRouter.js';
+import commentRoutes from './routes/commentRoutes.js';
 import morgan from 'morgan';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Configura CORS para permitir solicitudes desde tu frontend
+app.use(cors());
+
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(fileUpload({ createParentPath: true }));
 
 // Rutas para las API
 app.use('/api', menuRoutes);
@@ -27,15 +36,20 @@ app.use('/api', dishRoutes);
 app.use('/api', dessertRoutes);
 app.use('/api', drinkRoutes);
 app.use('/api', restaurantRoutes);
-app.use('/api', employeeRoutes)
-app.use('/api', inventoryRoutes)
-app.use('/api', userRoutes)
-app.use('/api', supplierRoutes)
-app.use('/api', food_typeRoutes)
-app.use('/api', reserveRoutes)
-app.use('/api', commentRoutes)
+app.use('/api', employeeRoutes);
+app.use('/api', inventoryRoutes);
+app.use('/api', userRoutes);
+app.use('/api', supplierRoutes);
+app.use('/api', food_typeRoutes);
+app.use('/api', reserveRoutes);
+app.use('/api', commentRoutes);
+
+// Ruta para servir archivos estáticos (como imágenes)
+app.use('/img/usuario', express.static(path.join(__dirname, 'public/img/usuario')));
+
+// Ruta para archivos estáticos generales (como CSS, JS, imágenes)
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
-    console.log(listEndpoints(app)); //! Lista de las rutas
 });
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,22 @@
-import { pool } from '../db.js'
+import { pool } from '../db.js';
+import bcrypt from 'bcrypt';
 
-//Crear usuario
-const createUser = async(nombreCompleto, correoElectronico, password, ruc, contacto) => {
-    const query = 'INSERT INTO users (nombreCompleto, correoElectronico, password, ruc, contacto) VALUES ($1, $2, $3, $4, $5) RETURNING *'
-    const { rows } = await pool.query(query, [ nombreCompleto, correoElectronico, password, ruc, contacto ])
-    return rows[0]
-}
+// Crear usuario
+const createUser = async (nombreCompleto, correoElectronico, password, ruc, contacto) => {
+    const hashedPassword = await bcrypt.hash(password, 8); // Hashear la contraseña
+    const query = 'INSERT INTO users (nombreCompleto, correoElectronico, password, ruc, contacto) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+    const { rows } = await pool.query(query, [nombreCompleto, correoElectronico, hashedPassword, ruc, contacto]);
+    return rows[0];
+};
+
+// Buscar usuario por correo electrónico
+const findUserByEmail = async (correoElectronico) => {
+    const query = 'SELECT * FROM users WHERE correoElectronico = $1';
+    const { rows } = await pool.query(query, [correoElectronico]);
+    return rows[0]; // Devuelve el primer usuario encontrado
+};
 
 export const userModel = {
-    createUser
-}
+    createUser,
+    findUserByEmail,
+};
