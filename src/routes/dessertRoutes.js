633d8b4a582ee95fe2ee4fd02c300459d749d0ef
Bsteven593 <bsm.latacumba@yavirac.edu.ec>
2025-02-05 10:24:34 -0500

    cambio a com estaba antes

--- src/routes/dessertRoutes.js
+++ src/routes/dessertRoutes.js
@@ -4,10 +4,10 @@ import { dessertController } from '../controllers/dessertController.js';
 const router = express.Router();
 
 // Rutas para Postres (Desserts) dentro de un usuario, un restaurante y un menú
-router.get('/restaurante/:restaurantId/menus/:menuId/desserts', dessertController.getDessertsByMenuId);
-router.get('/restaurante/:restaurantId/menus/:menuId/desserts/:id', dessertController.getDessertById);
-router.post('/restaurante/:restaurantId/menus/:menuId/desserts', dessertController.createDessert);
-router.put('/restaurante/:restaurantId/menus/:menuId/desserts/:id', dessertController.updateDessert);
-router.delete('/restaurante/:restaurantId/menus/:menuId/desserts/:id', dessertController.deleteDessert);
+router.get('/menus/:menuId/desserts', dessertController.getDessertsByMenuId);
+router.get('/menus/:menuId/desserts/:id', dessertController.getDessertById);
+router.post('/menus/:menuId/desserts', dessertController.createDessert);
+router.put('/menus/:menuId/desserts/:id', dessertController.updateDessert);
+router.delete('/menus/:menuId/desserts/:id', dessertController.deleteDessert);
 
 export default router;
--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -4,10 +4,10 @@ import { menuController } from '../controllers/menuController.js';
 const router = express.Router();
 
 // Rutas para menús asociados a un usuario y un restaurante
-router.get('/restaurante/:restaurantId/menus', menuController.getMenusByRestaurant);
-router.get('/restaurante/:restaurantId/menus/:id', menuController.getMenuById);
-router.post('/restaurante/:restaurantId/menus', menuController.createMenu);
-router.put('/restaurante/:restaurantId/menus/:id', menuController.updateMenu);
-router.delete('/restaurante/:restaurantId/menus/:id', menuController.deleteMenu);
+router.get('/menus', menuController.getMenusByRestaurant);
+router.get('/menus/:id', menuController.getMenuById);
+router.post('/menus', menuController.createMenu);
+router.put('/menus/:id', menuController.updateMenu);
+router.delete('/menus/:id', menuController.deleteMenu);
 
 export default router;
